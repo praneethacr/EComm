06cd787abac871a7bff044b0c3eb9d2272f3b174
Praneethacr <praneetha.c.r@gmail.com>
2019-05-09 14:40:45 -0700

    another commit

---
+++ EComm/EComm.Web/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EComm.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EComm.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error/{statusCode:int}")]
+        public IActionResult Index(int statusCode)
+        {
+            var evm = new ErrorViewModel();
+            ViewBag.StatusCode = statusCode;
+            return View("Error", evm);
+        }
+    }
+}
